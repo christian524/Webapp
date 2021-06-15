ee19d602cdcb30c836b40a00c3960b97ff1a5bcd
christian524 <79730887+christian524@users.noreply.github.com>
2021-06-14 21:14:29 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var firstname="Christian ";
+
+var lastname="Ketema";
+
+var fullname=firstname.concat(lastname);
+console.log(fullname);
+var length_of_fullname = fullname.length;
+console.log(length_of_fullname);
+var name_one = "My first Webpage";
+var replace_name = name_one.replace("Webpage", "Webapp");
+console.log(replace_name);
+var Capital_name = "CHRISTIAN KETEMA"
+var lowercase = Capital_name.toLowerCase();
+console.log(lowercase);
+var uppercase = lowercase.toUpperCase();
+console.log(uppercase);
+
+var squareroot = Math.sqrt(5);
+console.log(squareroot);
+var max_number = Math.max(123,456);
+console.log(max_number);
+var min_number = Math.min(123,456);
+console.log(min_number);
+var power_1 = Math.pow(8,4);
+console.log(power_1);
+var a = 4;
+var b = 8;
+var add = a+b;
+console.log(add)
